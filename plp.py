2638c42b2b424016c76608d5363a3ca8dcf15b3a
CaelumLaron <lucas.rocha@icomp.ufam.edu.br>
2019-11-12 17:56:41 -0400

    Lista 2

--- plp.py
+++ plp.py
@@ -1,6 +1,41 @@
-enc_consonantal = lambda s: 0 if len(s) < 2 else (s[0:2][0] not in 'aeiou' and s[0:2][1] not in 'aeiou') + enc_consonantal(s[1:])
-maisQueKEnc = lambda s, k: [i for i in s.split(' ') if enc_consonantal(i) > k]
-pelo_menos_k = lambda k, f, l: True if sum(f(i) for i in l) >= k else False
-baralho = lambda naipes, ranks: [(i, j) for i in naipes for j in ranks]
-semElementoK = lambda l, k: [l[i] for i in range(len(l)) if (i+1)!=k]
-print(semElementoK([1,2,3], 2))
+from socket import *
+from threading import *
+
+HOST = '127.0.0.1' #host name
+PORT = 7777 #comm port 
+
+#1) Abre dois terminais
+#2) Roda esse código em um
+#3) Conecta nele com telnet localhost 7777 no outro
+#4) Daí roda e vai testando
+
+class Server(Thread):
+	
+	def __init__(self):
+		Thread.__init__(self)
+		self.server = socket()
+		self.server.bind((HOST, PORT))
+		self.server.listen(5)
+		self.client, addr = self.server.accept()
+
+	def process_cmd(self, cmd):
+		reply = 'Done.\n'
+		return reply.encode()
+
+	def run(self):
+		print('Server started.')
+		while True:
+			try:
+				text = self.client.recv(1024)
+				print('Received')
+				if not text:
+					break
+				reply = self.process_cmd(text)
+				self.client.sendall(reply)
+			except:
+				print('Exception. Closed.')
+				break
+		self.client.close()
+
+if __name__ == '__main__':
+	app = Server().start()
